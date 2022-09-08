40315f23603b5b9396bfe9d1c6959449a87977fb
Pia <piatorain@yahoo.com>
2022-09-08 11:05:39 -0700

    reseeded db, landing page responsiveness

--- server/seed.js
+++ server/seed.js
@@ -17,7 +17,6 @@ module.exports = {
     seed: (req, res) => {
         sequelize.query(`
             drop table if exists comments;
-            drop table if exists authors;
             drop table if exists venues;
 
             CREATE TABLE venues (
@@ -30,15 +29,11 @@ module.exports = {
                 likes INTEGER
             );
 
-            CREATE TABLE authors (
-                author_id SERIAL PRIMARY KEY,
-                author_name VARCHAR(100)
-            );
 
             CREATE TABLE comments (
                 comment_id SERIAL PRIMARY KEY,
+                author VARCHAR(30),
                 text VARCHAR(500),
-                author_id INTEGER REFERENCES authors(author_id),
                 venue_id INTEGER REFERENCES venues(venue_id)
             );
 
@@ -47,16 +42,11 @@ module.exports = {
                 ('Bellagio Fountains', 'Attraction', 'The Bellagio Fountains are a set of dancing water fountains located on the Las Vegas Strip in front of the Bellagio hotel and casino.', 'https://i.ibb.co/ykvJqvS/bellagio-Fountains.jpg', 'https://bellagio.mgmresorts.com/en/entertainment/fountains-of-bellagio.html', 0),
                 ('The Venetian', 'Hotel', 'The Venetian Resort Hotel Casino is a luxury hotel and casino resort located on the Las Vegas Strip in Paradise, Nevada, United States. The resort is owned and operated by Las Vegas Sands.', 'https://i.ibb.co/qNKqWpQ/venetian.jpg', 'https://www.venetianlasvegas.com/', 0);
             
-            INSERT INTO authors (author_name) 
-                VALUES ('vegasBaby'),
-                ('newYorker4Life'),
-                ('Jane Q.'),
-                ('T. Donavan');
 
-            INSERT INTO comments (text, author_id, venue_id)
-                VALUES ('There are trails for every skillset here!', 1, 1),
-                ('I got married with the fountains as a backdrop.', 2, 2),
-                ('I love the gondolas!', 3, 3);
+            INSERT INTO comments (author, text, venue_id)
+                VALUES ('vegasBaby', 'There are trails for every skillset here!', 1),
+                ('newYorker4Life','I got married with the fountains as a backdrop.', 2),
+                ('Jane Q.', 'I love the gondolas!', 3);
 
                 
 
