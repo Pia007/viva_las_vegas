738fd71441bdecd15f8165f9184e0054fa8c8d68
Pia <piatorain@yahoo.com>
2022-09-13 09:11:59 -0700

    made inputs required

--- server/seed.js
+++ server/seed.js
@@ -49,7 +49,7 @@ module.exports = {
             INSERT INTO venues ( venue_name, type, details, author, image_url, website_url, likes) 
                 VALUES ( 'Red Rock Canyon', 'Hiking', 'Wow what a beautiful experience. We took the 13mi scenic drive through the canyon and stopped at different lookout point for photos and I even got my husband hike on a beginner trail.', 'Jane Q', 'https://i.ibb.co/dgx8s7W/redrock-Canyon.jpg', 'https://www.redrockcanyonlv.org/', 0),
                 ( 'Bellagio Fountains', 'Attraction', 'I absolutely love the fountains and how they are choreographed to some of my favorite songs. I, Highly recommended it!', 'newYorker4Life', 'https://i.ibb.co/ykvJqvS/bellagio-Fountains.jpg', 'https://bellagio.mgmresorts.com/en/entertainment/fountains-of-bellagio.html', 0),
-                ( 'The Venetian', 'Hotel', 'Top notch everything. I love to shop and I enjoyed the gondola with my hubs. Had a blast!', 'missTERY', 'https://i.ibb.co/qNKqWpQ/venetian.jpg', 'https://www.venetianlasvegas.com/', 0);
+                ( 'The Venetian', 'Hotel', 'Top notch everything. I love to shop and I enjoyed the gondola with my hubs. Had a blast!', 'missTERY', 'https://i.ibb.co/6g38ZMj/vene.jpg', 'https://www.venetianlasvegas.com/', 0);
             
 
         `).then(() => {
